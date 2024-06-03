f67d0e622ddfdcb817330eb8b118fd6559be72cd
VeshonaS <aniahshell1@gmail.com>
2024-06-03 13:22:01 -0400

    tech done

--- src/index.js
+++ src/index.js
@@ -2,5 +2,31 @@
  *
  */
 
-const handleSubmit = () => {
+const handleSubmit = (e) => {
+  e.preventDefault()
+  const userName = form.username.value
+  const codinglevel = form.codingLevel.value
+  const Location = form.location.value
+  const didEnjoy = form.didLikeAssignment.checked
+
+const newName = document.getElementById('results-username')
+newName.textContent = userName
+const levelres = document.getElementById('results-coding-level')
+levelres.textContent = codinglevel
+const urLocation = document.getElementById('results-location')
+urLocation.textContent = Location
+const enjoyment = document.getElementById('results-did-like-assignment')
+if (didEnjoy === true ){
+  enjoyment.textContent = "Yes"
+} else {
+  enjoyment.textContent ="No"
+}
+document.getElementById('new-user-form').reset()
 };
+
+const form = document.querySelector('#new-user-form')
+form.addEventListener('submit' , handleSubmit)
+
+
+
+

4c5d0713e00bf57fb5f087d144487267887992f5
Itzel01 <itzel.areymi.rendon@gmail.com>
2024-06-04 22:59:01 -0400

    feedback

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 /** GENERAL FEEDBACK:
- *
+ * HEY! Great job passing all of your test cases! I added an indent on some of your code so that the spacing was consistent!
  */
 
 const handleSubmit = (e) => {
@@ -9,19 +9,20 @@ const handleSubmit = (e) => {
   const Location = form.location.value
   const didEnjoy = form.didLikeAssignment.checked
 
-const newName = document.getElementById('results-username')
-newName.textContent = userName
-const levelres = document.getElementById('results-coding-level')
-levelres.textContent = codinglevel
-const urLocation = document.getElementById('results-location')
-urLocation.textContent = Location
-const enjoyment = document.getElementById('results-did-like-assignment')
-if (didEnjoy === true ){
-  enjoyment.textContent = "Yes"
-} else {
-  enjoyment.textContent ="No"
-}
-document.getElementById('new-user-form').reset()
+  const newName = document.getElementById('results-username')
+  newName.textContent = userName
+  const levelres = document.getElementById('results-coding-level')
+  levelres.textContent = codinglevel
+  const urLocation = document.getElementById('results-location')
+  urLocation.textContent = Location
+  const enjoyment = document.getElementById('results-did-like-assignment')
+  if (didEnjoy === true ){
+    enjoyment.textContent = "Yes"
+  } else {
+    enjoyment.textContent ="No"
+  }
+  /** FEEDBACK: Instead of re-targeting the form, you could just reset using the event! */
+  document.getElementById('new-user-form').reset()
 };
 
 const form = document.querySelector('#new-user-form')
